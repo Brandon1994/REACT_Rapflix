f4d6c604313da8a8259b15ebb53d85b13acd4d5a
Brandon1994 <brandon2_@hotmail.com>
2020-07-27 23:34:36 -0300

    Implenting the home page

---
+++ rapflix/src/components/Menu/components/ButtonLink/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+function ButtonLink(props) {
+  return (
+    <a className={props.className} href={props.href}>
+      {props.children}
+    </a>
+  );
+}
+
+export default ButtonLink;
